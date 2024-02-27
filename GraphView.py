7b2208a82b03cbce0282140413eca0b6b0ec663b
Marked <usssrflex@yandex.ru>
2024-02-28 02:45:37 +0400

    Добавить создание нового листа в exel

--- GraphView.py
+++ GraphView.py
@@ -102,6 +102,27 @@ def visualize_graph_and_save(G, pos, central_circle, middle_circle, outer_circle
     plt.savefig(output_file, dpi= 500)
     plt.close()
 
+def create_new_sheet_with_statistics(file_path):
+    # Считываем данные
+    df = pd.read_excel(file_path, sheet_name='Лист1', index_col=0)
+
+    # Вычисляем статистики
+    V_Plus, B_Plus, emotional_expansiveness, sociometric_status = calculate_statistics(df)
+
+    # Создаем новый лист с именем 'Статистика'
+    with pd.ExcelWriter(file_path, engine='openpyxl', mode='a') as writer:
+        # Если файл уже существует, добавляем к нему новый лист
+        new_sheet_name = 'Статистика'
+        df_statistics = pd.DataFrame({
+            'V_Plus': V_Plus,
+            'B_Plus': B_Plus,
+            'Emotional_Expansiveness': emotional_expansiveness,
+            'Sociometric_Status': sociometric_status
+        }, index=df.index)
+
+        df_statistics.to_excel(writer, sheet_name=new_sheet_name)
+
+
 
 def main():
     # Путь к файлу Excel
@@ -113,7 +134,6 @@ def main():
 
     # Вычисляем статистики
     V_Plus, B_Plus, emotional_expansiveness, sociometric_status = calculate_statistics(df)
-    print(V_Plus, B_Plus, emotional_expansiveness, sociometric_status, sep='\n')
     pers_data(df)
     S_group(df)
 
@@ -152,4 +172,6 @@ def main():
 
 
 if __name__ == "__main__":
+    file_path = r"D:\Unik\TerVer\data\Книга1_тест.xlsx"
+    create_new_sheet_with_statistics(file_path)
     main()
