53098f39a57b5697ed2f947456c20d080450bb3b
Marked <usssrflex@yandex.ru>
2024-03-02 14:22:51 +0400

    Исправления бага с созданием нового файла

--- GraphView.py
+++ GraphView.py
@@ -86,7 +86,7 @@ def visualize_graph_and_save(G, pos, central_circle, middle_circle, outer_circle
     plt.close()
 
 
-def create_new_sheet_with_statistics(input_file_path, output_file_path, img_path):
+def create_new_sheets_with_statistics(input_file_path, output_file_path, img_path):
     """
     Создает новый файл Excel, добавляет в него статистику и изображение, сохраняет результат.
 
@@ -95,36 +95,41 @@ def create_new_sheet_with_statistics(input_file_path, output_file_path, img_path
     - output_file_path (str): Путь к создаваемому файлу Excel с добавленной статистикой.
     - img_path (str): Путь к изображению для вставки в созданный файл Excel.
     """
-    df = pd.read_excel(input_file_path, sheet_name='Лист1', index_col=0)
-    S_group, Э_group, BB_group, C_plus, Э_plus, KUO = calculate_statistics(df)
-
     if os.path.exists(output_file_path):
         os.remove(output_file_path)
 
     with pd.ExcelWriter(output_file_path, engine='openpyxl') as writer:
-        df.to_excel(writer, sheet_name='Лист1')
+        for sheet_number in range(1, 7):
+            df = pd.read_excel(input_file_path, sheet_name=f'Лист{sheet_number}', index_col=0)
+            S_group, Э_group, BB_group, C_plus, Э_plus, KUO = calculate_statistics(df)
 
-        new_sheet_name = 'Статистика'
-        df_statistics = pd.DataFrame({
-            'S_group': [S_group],
-            'Э_group': [Э_group],
-            'BB_group': [BB_group],
-        })
+            df.to_excel(writer, sheet_name=f'Лист{sheet_number}')  # Сохраняем исходный лист
 
-        df_statistics.to_excel(writer, sheet_name=new_sheet_name, startcol=df.shape[1] - 10, startrow = 12)
+            new_sheet_name = f'Статистика{sheet_number}'
+            df_statistics = pd.DataFrame({
+                'S_group': [S_group],
+                'Э_group': [Э_group],
+                'BB_group': [BB_group],
+            })
 
-        df_extended = pd.DataFrame({
-            'C_plus': C_plus,
-            'Э_plus': Э_plus,
-            'КУО': KUO,
-        }, index=df.index)
+            # Identify the appropriate start coordinates
+            startcol = max(df.shape[1] - 21, 1)
+            startrow = 0
 
-        df_extended.to_excel(writer, sheet_name=new_sheet_name)
+            df_statistics.to_excel(writer, sheet_name=new_sheet_name, startcol=startcol, startrow=startrow)
 
-        workbook = writer.book
-        sheet = workbook[new_sheet_name]
-        img = Image(img_path)
-        sheet.add_image(img, 'I1')
+            df_extended = pd.DataFrame({
+                'C_plus': C_plus,
+                'Э_plus': Э_plus,
+                'КУО': KUO,
+            }, index=df.index)
+
+            df_extended.to_excel(writer, sheet_name=new_sheet_name)
+
+            workbook = writer.book
+            sheet = workbook[new_sheet_name]
+            img = Image(img_path)
+            sheet.add_image(img, 'I1')
 
 
 def main(input_file_path, output_file_path, output_image_file):
@@ -139,37 +144,36 @@ def main(input_file_path, output_file_path, output_image_file):
 
     shutil.copy(input_file_path, output_file_path)
 
-    df = pd.read_excel(output_file_path, sheet_name='Лист1', index_col=0) #### ИМЯ ЛИСТА ПОМЕНЯТЬ!
-    print(df)
 
+    for sheet_number in range(1, 7):
+        df = pd.read_excel(output_file_path, sheet_name=f'Лист{sheet_number}', index_col=0)
 
-    G = create_directed_graph(df)
+        G = create_directed_graph(df)
 
-    node_degrees = dict(G.in_degree())
-    sorted_nodes = sorted(node_degrees, key=node_degrees.get, reverse=True)
+        node_degrees = dict(G.in_degree())
+        sorted_nodes = sorted(node_degrees, key=node_degrees.get, reverse=True)
 
-    num_groups = 3
-    nodes_per_group = len(sorted_nodes) // num_groups
-    remainder = len(sorted_nodes) % num_groups
+        num_groups = 3
+        nodes_per_group = len(sorted_nodes) // num_groups
+        remainder = len(sorted_nodes) % num_groups
 
-    central_circle = sorted_nodes[:nodes_per_group + remainder]
-    middle_circle = sorted_nodes[nodes_per_group + remainder:2 * nodes_per_group + remainder]
-    outer_circle = sorted_nodes[2 * nodes_per_group + remainder:]
+        central_circle = sorted_nodes[:nodes_per_group + remainder]
+        middle_circle = sorted_nodes[nodes_per_group + remainder:2 * nodes_per_group + remainder]
+        outer_circle = sorted_nodes[2 * nodes_per_group + remainder:]
 
-    pos = {}
-    for i, group in enumerate([central_circle, middle_circle, outer_circle]):
-        theta = np.linspace(0, 2 * np.pi, len(group), endpoint=False)
-        radius = (i + 1) * 5
-        x = radius * np.cos(theta)
-        y = radius * np.sin(theta)
+        pos = {}
+        for i, group in enumerate([central_circle, middle_circle, outer_circle]):
+            theta = np.linspace(0, 2 * np.pi, len(group), endpoint=False)
+            radius = (i + 1) * 5
+            x = radius * np.cos(theta)
+            y = radius * np.sin(theta)
 
-        for node, (xx, yy) in zip(group, zip(x, y)):
-            pos[node] = (xx, yy)
+            for node, (xx, yy) in zip(group, zip(x, y)):
+                pos[node] = (xx, yy)
 
-    visualize_graph_and_save(G, pos, central_circle, middle_circle, outer_circle, output_image_file)
+        visualize_graph_and_save(G, pos, central_circle, middle_circle, outer_circle, output_image_file)
 
-    create_new_sheet_with_statistics(input_file_path, output_file_path, output_image_file)
-
+        create_new_sheets_with_statistics(input_file_path, output_file_path, output_image_file)
 
 if __name__ == "__main__":
     # input_file_path = input('Путь к исходному файлу Excel -> ')
@@ -193,6 +197,6 @@ if __name__ == "__main__":
         for excel_file in excel_files:
             input_file_path = os.path.join(data_directory, excel_file)
             output_file_path = os.path.join(output_directory, f'Выходные данные_{excel_file}')
-            output_image_file = os.path.join(output_directory, f'Meoww1_{excel_file.replace(".xlsx", ".png")}')
+            output_image_file = os.path.join(output_directory, f'Graph_{excel_file.replace(".xlsx", ".png")}')
 
             main(input_file_path, output_file_path, output_image_file)
